22135f54771cc7c7c6a1229d007f95ce1fe00634
Globalika <volodymyr.seredovych@gmail.com>
2020-08-14 11:47:27 +0300

    event system

--- Globalika/src/Globalika/Application.cpp
+++ Globalika/src/Globalika/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Globalika/Events/ApplicationEvent.h"
+#include "Globalika/Log.h"
+
 namespace Globalika {
 	Application::Application()
 	{
@@ -10,6 +13,8 @@ namespace Globalika {
 	}
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		GLOB_TRACE(e);
 		while (true);
 	}
 }
--- premake5.lua
+++ premake5.lua
@@ -26,6 +26,7 @@ project "Globalika"
 
 	includedirs
 	{
+		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include"
 	}
 
